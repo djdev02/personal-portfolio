ed7672f3caacbdc02d11cde1180a70d4820dc9f5
debjz <72181048+anishz1@users.noreply.github.com>
2022-07-22 18:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// menubutton 
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+const toggleNavbar = ()=>{
+   menu.classList.toggle("active");
+};
+
+menuBtn.addEventListener("click",()=>toggleNavbar());
+
+// sticky navigation menu 
+let nav=document.querySelector(".navbar");
+let val;
+window.onscroll=function(){
+   if(document.documentElement.scrollTop>20){
+      nav.classList.add("sticky");
+   }
+   else{
+      nav.classList.remove("sticky");
+   }
+}
+
+// typing animation 
+var typed= new Typed(".typing",{
+   strings:["Developer","Designer","Freelancer"],
+   typeSpeed: 100,
+   backSpeed: 60,
+   loop: true
+});
+
+//  form validation
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const subject  = document.getElementById('subject')
+const textArea  = document.getElementById('textarea')
+const errorElement =document.getElementById('error_message')
+
+myform.addEventListener('submit',(e) =>{
+  let messages=[]
+  if(name.value === '' || name.value == null){
+   messages.push('Name is required')
+  }
+  if (subject.value.length <=6){
+   messages.push('subject must be longer than 6 characters')
+  }
+  if (subject.value.length >=20){
+   messages.push('subject must be less than 20 characters')
+  }
+ 
+  if (textArea.value.length <=27){
+   messages.push('Description must be longer than 30 characters')
+  }
+
+  
+  if(messages.length>0){
+     e.preventDefault()
+   errorElement.innerText = messages.join(', ')
+  }
+
+})
