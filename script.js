4790b126ac029155f5923b267ffcc68f50ebd1e0
anishz1 <debashishj18@gmail.com>
2023-04-26 17:36:40 +0530

    Update form validation

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ var image = document.querySelector("#page>img");
 
 main.addEventListener("mousemove",function(dets){
   image.style.top = 1-dets.y*0.05 + "px"
-console.log(dets.x)
-console.log(dets.y)
+// console.log(dets.x)
+// console.log(dets.y)
   image.style.left = 1-dets.x *0.05+ "px" 
 })
 
